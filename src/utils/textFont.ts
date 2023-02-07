04f4dfaaf5b25ec3ef159734dfe86f5d5a8ece1b
Laís Martins <silveiramartinslais@gmail.com>
2023-02-07 08:57:18 -0300

    textFont configured

---
+++ src/utils/textFont.ts
@@ -0,0 +1,8 @@
+import { Source_Sans_Pro } from "@next/font/google";
+
+const SourceSansPro = Source_Sans_Pro({
+  weight: ["400", "600", "700"],
+  subsets: ["latin"],
+});
+
+export default SourceSansPro;
